19bf0a5a97611eb039616be3790983bbb442f275
Dipesh Satav <38184559+KiloSat@users.noreply.github.com>
2021-07-26 14:08:43 +0530

    requests.get(url).json updated

--- apps/technical_analysis.py
+++ apps/technical_analysis.py
@@ -31,7 +31,7 @@ def app():
     
     def get_symbol(symbol):
         url = "http://d.yimg.com/autoc.finance.yahoo.com/autoc?query={}&region=1&lang=en".format(symbol)
-        result = requests.get(url)
+        result = requests.get(url).json()
         for x in result['ResultSet']['Result']:
             if x['symbol'] == symbol:
                 return x['name']
